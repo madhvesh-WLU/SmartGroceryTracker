a247d93d93c995e02f6b9af6fe69001ad9dbd71f
payal <darj6950@mylaurier.ca>
2024-10-26 14:07:47 -0400

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
-    implementation(libs.activity)
+   // implementation(libs.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
